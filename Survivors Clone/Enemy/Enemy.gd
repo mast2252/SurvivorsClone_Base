5e7f4bb19770bb63f77a0cc7b1ccbe6d565b9e42
hambj <jeremie.hambye@actito.com>
2023-02-12 23:32:31 +0100

    Video 3

--- Survivors Clone/Enemy/Enemy.gd
+++ Survivors Clone/Enemy/Enemy.gd
@@ -2,12 +2,18 @@ extends CharacterBody2D
 
 @export var movement_speed:float = 20.0
 
+@onready var sprite = $Sprite2D
 @onready var player:CharacterBody2D = get_tree().get_first_node_in_group("players");
+@onready var animation:AnimationPlayer = $AnimationPlayer;
+
+func _ready():
+	animation.play("walk")
 
 func _physics_process(_delta):
 	
-	$Sprite2D.flip_h = player.global_position.x > global_position.x
+	sprite.flip_h = player.global_position.x > global_position.x
 
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction*movement_speed;
 	move_and_slide()
+	
--- Survivors Clone/Player/Player.gd
+++ Survivors Clone/Player/Player.gd
@@ -1,17 +1,25 @@
 extends CharacterBody2D
 
 var movement_speed:float = 40.0
+@onready var sprite:Sprite2D = $Sprite2D
+@onready var walk_timer:Timer = get_node("%walkTimer")
 
-func _process(delta):
-	pass
-	
 func _physics_process(_delta):
 	movement()
 	
 func movement():
 	var  x_mov = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var  y_mov = Input.get_action_strength("down") - Input.get_action_strength("up")
+	if (x_mov != 0):
+		sprite.flip_h = x_mov > 0
 	var mov = Vector2(x_mov, y_mov).normalized()
+	
+	if (mov != Vector2.ZERO && walk_timer.is_stopped()):
+		if sprite.frame >= sprite.hframes - 1:
+			sprite.frame = 0
+		else:
+			sprite.frame +=1
+		walk_timer.start()
 	velocity = mov*movement_speed
 	move_and_slide()
 	
